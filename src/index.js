af77ffcab5e8645fb69a5055f137e698008fb756
Ryan Joseph <ryan.joseph@gmail.com>
2024-08-18 01:32:01 +0000

    Initial working version

--- src/index.js
+++ src/index.js
@@ -1,15 +1,37 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import puppeteer from "@cloudflare/puppeteer";
 
 export default {
-	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
+	async fetch(request, env) {
+		const { searchParams } = new URL(request.url);
+		let url = searchParams.get("url");
+		if (url) {
+			url = new URL(url).toString();
+			const browser = await puppeteer.launch(env.BROWSER);
+			const page = await browser.newPage();
+			console.log(`Browsing to ${url}...`);
+			await page.goto(url, {
+				waitUntil: 'networkidle2',
+			});
+			const name = 'foobarbaz.pdf';
+			console.log(`Rendering ${url} as PDF...`);
+			const pdfBytes = await page.pdf({
+				format: 'letter',
+			});
+			await browser.close();
+			console.log(`Rendered ${pdfBytes.length} bytes...`);
+			const { etag } = await env.PERSIST_BUCKET.put(name, pdfBytes);
+			console.log(`Persisted ${url} as ${name} (etag=${etag})`);
+			return new Response(pdfBytes, {
+				headers: {
+					"content-type": "application/pdf",
+					"r2-name": name,
+					"r2-etag": etag,
+				},
+			});
+		} else {
+			return new Response(
+				"Please add an ?url=https://example.com/ parameter"
+			);
+		}
 	},
 };
